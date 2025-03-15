9252550643e88c6c05034af1ba0c7c7c6f36714f
albert-ward <albertjamesward@gmail.com>
2025-03-15 14:51:59 +0000

    Deploying to gh-pages from @ albert-ward/albert-ward.github.io@2b6d6d8c9755b739b187f27ff6cecbe7564133c4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,13 +37,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/teaching/";
           },
-        },{id: "nav-submenus",
-          title: "submenus",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/_pages/dropdown.html";
-          },
         },{
         id: 'social-email',
         title: 'email',
