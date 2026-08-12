409029ce4af5a8d6eb613e9723a7fbdcb63c0094
albert-ward <albertjamesward@gmail.com>
2026-08-12 15:19:59 +0000

    Deploying to gh-pages from @ albert-ward/albert-ward.github.io@2ae3c149bc4a5729707159e4e4a7de7970c182b1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -45,41 +45,6 @@ ninja.data = [{
             window.location.href = "/teaching/";
           },
         },{
-        id: 'social-email',
-        title: 'email',
-        section: 'Socials',
-        handler: () => {
-          window.open("mailto:%61%6C%62%65%72%74%6A%61%6D%65%73%77%61%72%64@%67%6D%61%69%6C.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-linkedin',
-        title: 'LinkedIn',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.linkedin.com/in/www.linkedin.com/in/albertjward", "_blank");
-        },
-      },{
-        id: 'social-orcid',
-        title: 'ORCID',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://orcid.org/0000-0002-3221-5415", "_blank");
-        },
-      },{
-        id: 'social-osf',
-        title: 'Open Science Framework',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://osf.io/https://osf.io/xqdpr//", "_blank");
-        },
-      },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=ISG6aFQAAAAJ&hl", "_blank");
-        },
-      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
